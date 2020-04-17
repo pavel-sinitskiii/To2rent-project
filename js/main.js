f43cad432d25634d67c2f7226c72a1cf07c61aeb
pavel.sinitskiii <pavel.sinitskiii@gmail.com>
2020-04-17 22:11:21 +0300

    to2rent

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+$('.slider').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
+    fade:true,
+    responsive: [
+      {
+        breakpoint: 441,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+   
+});
+
+new WOW().init();
+
+
+
+
+
+
+
+
